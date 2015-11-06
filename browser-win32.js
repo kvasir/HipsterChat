7b54384b8442d8a4141255b99f492eff6c0065ab
John-Philip Johansson <john-philip.johansson@episerver.com>
2015-11-06 18:14:35 +0100

    Add numbered badges

--- browser-win32.js
+++ browser-win32.js
@@ -1,18 +1,75 @@
 'use strict';
 console.log('browser-win32.js loaded');
 
+const ipc = require('ipc');
+const remote = require('remote');
+const NativeImage = remote.require('native-image');
+const app = remote.require('app');
+const mainWindow = remote.getCurrentWindow();
+
 // Electron doesn't support notifications in Windows yet. https://github.com/atom/electron/issues/262
 // So we hijack the Notification API.
-const ipc = require('ipc');
 const OldNotification = Notification;
+let notificationCounter = 0;
 
 Notification = function (title, options) {
 	const notification = new OldNotification(title, options);
+
 	ipc.send('notification-shim', {
 		title,
 		options
 	});
 
+	notificationCounter++;
+	setBadge(notificationCounter);
+
 	return notification;
 };
 Notification.prototype = OldNotification.prototype;
+
+ipc.on('reset-notifications', () => removeBadge());
+
+function removeBadge() {
+	notificationCounter = 0;
+	mainWindow.setOverlayIcon(null, '');
+}
+
+function setBadge() {
+	let text = notificationCounter.toString();
+	if (process.platform === 'darwin') {
+		app.dock.setBadge(text);
+	} else if (process.platform === 'win32') {
+		if (text === '') {
+			mainWindow.setOverlayIcon(null, '');
+			return;
+		}
+
+		// Create badge
+		const canvas = document.createElement('canvas');
+		canvas.height = 140;
+		canvas.width = 140;
+		const ctx = canvas.getContext('2d');
+		ctx.fillStyle = 'red';
+		ctx.beginPath();
+		ctx.ellipse(70, 70, 70, 70, 0, 0, 2 * Math.PI);
+		ctx.fill();
+		ctx.textAlign = 'center';
+		ctx.fillStyle = 'white';
+
+		if (text.length > 2) {
+			ctx.font = 'bold 65px "Segoe UI", sans-serif';
+			ctx.fillText(text, 70, 95);
+		} else if (text.length > 1) {
+			ctx.font = 'bold 85px "Segoe UI", sans-serif';
+			ctx.fillText(text, 70, 100);
+		} else {
+			ctx.font = 'bold 100px "Segoe UI", sans-serif';
+			ctx.fillText(text, 70, 105);
+		}
+
+		const badgeDataURL = canvas.toDataURL();
+		const img = NativeImage.createFromDataUrl(badgeDataURL);
+
+		mainWindow.setOverlayIcon(img, text);
+	}
+}
--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const Tray = require('tray');
 const shell = require('shell');
 const ipc = require('ipc');
 const notifier = require('node-notifier');
-const NativeImage = require('native-image');
 const appMenu = require('./menu');
 
 // report crashes to the Electron project
@@ -17,7 +16,6 @@ require('crash-reporter').start();
 // adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
 
-const badge = NativeImage.createFromPath(path.join(__dirname, 'media/dot.png'));
 const windows = [];
 let tray;
 let lastActiveWindow;
@@ -58,22 +56,6 @@ function showNotification(title, content) {
 	});
 }
 
-function showBadge() {
-	if (process.platform === 'darwin') {
-		app.dock.setBadge(badge);
-	} else if (process.platform === 'win32') {
-		lastActiveWindow.setOverlayIcon(badge, 'You have unread messages');
-	}
-}
-
-function hideBadge() {
-	if (process.platform === 'darwin') {
-		app.dock.setBadge('');
-	} else if (process.platform === 'win32') {
-		lastActiveWindow.setOverlayIcon(null, '');
-	}
-}
-
 function createTeamWindow(team) {
 	const win = new BrowserWindow({
 		'min-width': 750,
@@ -95,7 +77,7 @@ function createTeamWindow(team) {
 	});
 
 	win.loadUrl(`https://${team}.hipchat.com/chat`);
-	win.on('focus', () => hideBadge());
+	win.on('focus', () => win.webContents.send('reset-notifications'));
 	win.webContents.on('new-window', (e, url) => {
 		e.preventDefault();
 		shell.openExternal(url);
@@ -154,14 +136,12 @@ app.on('ready', () => {
 
 				tray.on('balloon-clicked', () => {
 					lastActiveWindow.focus();
-					hideBadge();
 				});
 			}
 
 			if (settings.win32.notificationBoxes) {
 				notifier.on('click', () => {
 					lastActiveWindow.focus();
-					hideBadge();
 				});
 			}
 
@@ -173,7 +153,6 @@ app.on('ready', () => {
 
 				showBalloon(msg.title, msg.options.body);
 				showNotification(msg.title, msg.options.body);
-				showBadge();
 
 				// Doesn't seem possible to pass extra info to balloon, and we want this window to open when the balloon is clicked.
 				lastActiveWindow = win;
